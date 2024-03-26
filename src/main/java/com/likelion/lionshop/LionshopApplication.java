d762006e1e878665c8579185470c348cc3432473
baeksonghyun <bsh0374@gmail.com>
2024-03-26 23:47:32 +0900

    1주차 과제

---
+++ src/main/java/com/likelion/lionshop/LionshopApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.lionshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LionshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LionshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/likelion/lionshop/LionshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.lionshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LionshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
